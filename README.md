a676931d9ff2f5b257d8abd03080f98dc01eb949
Rustam Kuramshin <leo.in.empty@gmail.com>
2021-01-07 18:15:31 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. ```npm install```
+
+2. ```node index.js --interval 5000```
---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+'use strict'
+
+// Импорт зависимостей
+const argv = require('yargs').argv;
+const SimpleNodeLogger = require('simple-node-logger');
+const request = require('request');
+
+// Настройка логера
+const opts = {
+    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+};
+const log = SimpleNodeLogger.createSimpleLogger(opts);
+
+// Параметры запроса
+const reqOptions = {
+    method: 'POST',
+    url: 'https://fcm.googleapis.com/fcm/send',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': ''
+    },
+    body: JSON.stringify({
+        'to': '',
+        'content_available':true,
+        'apns-priority':5,
+        'data':{
+            'some-key':"some-value"}
+    })
+};
+
+(async () => {
+
+    log.info('Начало выполнения скрипта');
+
+    setInterval(() => {
+
+        log.info('Делаем POST-запрос');
+
+        request(reqOptions, (err, res) => {
+            if (err) {
+                log.error(`Запрос закончился ошибкой: ${err}`);
+                // throw new Error(err);
+            } else {
+                log.info(`Ответ получен: ${res.body}`);
+            }
+        })
+
+    }, parseInt(argv.interval));
+
+})();
